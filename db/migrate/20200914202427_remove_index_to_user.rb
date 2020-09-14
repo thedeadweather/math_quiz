2018d06a6e622debc776630e3977dc0c0cf81058
Alex Kln <kalininmalinin@gmail.com>
2020-09-15 00:13:08 +0300

    edit views

---
+++ db/migrate/20200914202427_remove_index_to_user.rb
@@ -0,0 +1,5 @@
+class RemoveIndexToUser < ActiveRecord::Migration[6.0]
+  def change
+    remove_index :users, name: :index_users_on_reset_password_token
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_134936) do
+ActiveRecord::Schema.define(version: 2020_09_14_202427) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "finished_at"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_09_14_134936) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
-    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
