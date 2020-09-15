97045f1aa810f60e07d0ee81ac4f8357dabb305d
Alex Kln <kalininmalinin@gmail.com>
2020-09-15 03:08:58 +0300

    edit db for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_202427) do
+ActiveRecord::Schema.define(version: 2020_09_15_000557) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "finished_at"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_202427) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
+    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
